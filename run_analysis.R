80e6a183ff198ddff0517cab79c7acaaff50a88b
SSwan13 <sswan@veriskhealth.com>
2016-01-22 20:18:57 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,7 @@
 library(reshape2)
 library(dplyr)
 
-## STEP 1: Merges the training and the test sets to create one data set
-
-# read data into data frames
+# read data from files into data frames
 subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
@@ -12,7 +10,7 @@ y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
 features <- read.table("./data/UCI HAR Dataset/features.txt")
 
-# rename columns 
+# rename columns within train and test data sets
 # subject column given a more descriptive label than V1
 names(subject_train) <- "subjectId"
 names(subject_test) <- "subjectId"
@@ -23,41 +21,32 @@ names(x_test) <- features$V2
 names(y_train) <- "activityId"
 names(y_test) <- "activityId"
 
-# combine files into one data set
+# merge the train and test sets to create one working data set
 train_ds <- cbind(subject_train, y_train, x_train)
 test_ds <- cbind(subject_test, y_test, x_test)
 ds <- rbind(train_ds, test_ds)
 
 
-## STEP 2: Extracts only the measurements on the mean and standard deviation for each measurement.
-
 # determine which columns contain "mean" or "std"
 mean_std <- grepl("mean", names(ds)) | grepl("std", names(ds))
 
-# ensure that we also keep the subjectId and activityId columns
+# need to keep the subjectId and activityId columns
 mean_std[1:2] <- TRUE
 
-# remove unnecessary columns - only want the mean and std columns
+# remove unnecessary columns - only want to extract the mean and standard deviation columns
 ds <- ds[, mean_std]
 
 
-## STEP 3: Uses descriptive activity names to name the activities
-## in the data set.
-## STEP 4: Appropriately labels the data set with descriptive
-## variable names. 
+# add column with a more descriptive name for activity
+ds <- mutate(ds, activityName = factor(ds$activityId, labels=c("Walking", "Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying")) )
 
-# add column with activity name
-ds <- mutate(ds, activityName = factor(ds$activityId, labels=c("Walking","Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying")) )
-
-#drop activityId column (id no longer needed in tidy set of data since have activity name)
+#drop activityId column (id no longer needed in working set of data since have activity name now)
 ds$activityId <- NULL
 
-## STEP 5: Creates a second, independent tidy data set with the
-## average of each variable for each activity and each subject.
 
-# create the tidy data set
+# create a new data set with the average of each variable for each activity and each subject.
 melted <- melt(ds, id=c("subjectId","activityName"))
 tidy_ds <- dcast(melted, subjectId+activityName ~ variable, mean)
 
-# write the tidy data set to a file
+# write the new data set out to a file
 write.csv(tidy_ds, "tidy_ds.txt", row.names=FALSE)
